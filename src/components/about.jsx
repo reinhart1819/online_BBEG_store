876818e279548f80820420ea2e619fd18a60fa1a
Christopher Reinhart <reinhart1819@gmail.com>
2022-06-30 13:32:52 -0700

    mosnster final

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 const About = () => {
+  let [visible, setVisible] = useState(false);
+  const changeVis = () => {
+    if (visible) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+  };
+
   return (
     <div>
       <h1>About me</h1>
       <h3>Chris Reinhart</h3>
+      {visible ? <h4>chris@email.com</h4> : "Click on the button to"}
+      <button className="btn btn-primary btn-sm" onClick={changeVis}>
+        Show email
+      </button>
     </div>
   );
 };
 
 export default About;
+
+// alt we could use onClick{()=> setVisible(true)}   as an inline function
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -9,12 +9,17 @@ const Product = (props) => {
     setQuanity(val);
   };
 
+  const getTotal = () => {
+    let total = props.data.price * quanity;
+    return total.toFixed(2);
+  };
+
   return (
     <div className="product">
       <img src={"/img/" + props.data.image} alt="" />
       <h2>{props.data.title}</h2>
-      <label>Price: ${props.data.price} Thousand</label>
-      <label>Total: ${(props.data.price * quanity).toFixed(2)} Thousand </label>
+      <label>Price: {props.data.price.toFixed(2)} Thousand gp</label>
+      <label>Total: {getTotal()} Thousand gp</label>
       <QuanityPicker onChange={quantityChange}></QuanityPicker>
       <button className="btn btn-primary btn-sm">Add</button>
     </div>
